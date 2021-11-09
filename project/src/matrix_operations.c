f763f1464250e310e9539caed3dd3a94253f0be8
Viktor <vityakiselev96@gmail.com>
2021-11-09 09:17:52 +0300

    Add transposition in sum function

--- project/src/matrix_operations.c
+++ project/src/matrix_operations.c
@@ -60,6 +60,60 @@ int fill_matrix(double** matrix, int rows_quanity, int columns_quanity) {
     }
     return 0;
 }
-int sum_int(int a, int b) {
-    return a+b;
+
+
+
+
+
+// double sum_row(double* row, int columns_quanity) {
+//     double sum = 0;
+//     for (int i = 0; i < columns_quanity; ++i) {
+//         sum += row[i];
+//     }
+//     return sum;
+// }
+
+// int sum_rows(double * columns_sum, double ** matrix, int rows_quanity, int columns_quanity) {
+//     if(columns_sum == NULL) return error_row_sum;
+//     if(matrix == NULL) return error_with_matrix;
+//     for(int i = 0; i < rows_quanity; ++i){
+//         columns_sum[i] = sum_row(matrix[i], columns_quanity);
+//     }
+//     // for(int i =0; i< columns_quanity; ++i) {
+//     //     rows_sum[i] = sum_column(matrix, rows_quanity, i);
+//     // }
+//     return succes;
+// }
+
+// void copy_matrix(double **matrix_source, double **matrix_receiver, int rows_quanity, int columns_quanity){
+//     for(int i = 0; i < rows_quanity; ++i){
+//         for(int k = 0; k < columns_quanity; ++k){
+//             matrix_receiver[i][k] = matrix_source[i][k];
+//         };
+//     }
+// }
+// int transpose(double ****matrix, int rows_quanity, int columns_quanity){
+//     if(**matrix == NULL) return error_with_matrix;
+//     double **transposed_matrix = NULL;
+//     init_matrix(&transposed_matrix, columns_quanity, rows_quanity);
+//     for (int i = 0; i < rows_quanity; ++i){
+//         for (int k = 0; k < columns_quanity; ++k){
+//             transposed_matrix[k][i] = (**matrix)[i][k];
+//         }
+//     }
+//     free_matrix(*matrix,rows_quanity);
+//     init_matrix(*matrix,rows_quanity, columns_quanity);
+//     copy_matrix(transposed_matrix, (**matrix), columns_quanity, rows_quanity);
+//     free_matrix(&transposed_matrix, columns_quanity);
+//     return succes;
+// }
+int transpose(double **matrix, double **transposed_matrix, int rows_quanity, int columns_quanity){
+    if(matrix == NULL) return error_with_matrix;
+    if(transposed_matrix == NULL) return error_with_matrix;
+    for (int i = 0; i < rows_quanity; ++i){
+        for (int k = 0; k < columns_quanity; ++k){
+            transposed_matrix[k][i] = matrix[i][k];
+        }
+    }
+    return succes;
 }
